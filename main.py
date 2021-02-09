e92fe04ad0303f6b3343999bac20ba7dd9c9863c
Ivan0042 <76991671+Ivan0042@users.noreply.github.com>
2021-02-09 19:10:25 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import requests
 
 api_server = "http://static-maps.yandex.ru/1.x/"
 
-lon = input()
-lat = input()
-delta = input()
+lon = '0'
+lat = '0'
+delta = '90.0'
 
 params = {"ll": ",".join([lon, lat]),
           "spn": ",".join([delta, delta]),
@@ -20,10 +20,34 @@ with open(map_file, "wb") as file:
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                if float(delta) < 80:
+                    delta = str(float(delta) + 10)
+            if event.key == pygame.K_PAGEDOWN:
+                if float(delta) > 10:
+                    delta = str(float(delta) - 10)
+            if event.key == pygame.K_RIGHT:
+                lon = str(float(lon) + 3)
+            if event.key == pygame.K_LEFT:
+                lon = str(float(lon) - 3)
+            if event.key == pygame.K_DOWN:
+                lat = str(float(lat) - 3)
+            if event.key == pygame.K_UP:
+                lat = str(float(lat) + 3)
+    params = {"ll": ",".join([lon, lat]),
+              "spn": ",".join([delta, delta]),
+              "l": "map"}
+    response = requests.get(api_server, params=params)
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+pygame.quit()
 os.remove(map_file)
